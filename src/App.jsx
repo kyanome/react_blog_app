14ed863574e20ac1b8bd80136f75a471e4ba8879
kyanome <yanome.keita@gmail.com>
2025-02-15 17:05:05 +0900

    Add postcard component to home view

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import Header from './components/Header'
+import Home from './pages/Home'
 
 function App() {
 
   return (
     <>
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+      </Routes>
     </>
   )
 }
