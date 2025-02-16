28aedf7ad6a2455fd21b1992762f2ec850faea8d
kyanome <yanome.keita@gmail.com>
2025-02-16 11:46:04 +0900

    fix : wrap multiple elements in App.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,15 +4,13 @@ import Layout from "../components/Layout";
 
 function Home() {
   return (
-    <Layout>
-      <ul className="space-y-4">
-        {posts.map((post) => (
-          <li key={post.id}>
-            <PostCard post={post} />
-          </li>
-        ))}
-      </ul>
-    </Layout>
+    <ul className="space-y-4">
+      {posts.map((post) => (
+        <li key={post.id}>
+          <PostCard post={post} />
+        </li>
+      ))}
+    </ul>
   );
 }
 
--- src/pages/PostDetail.jsx
+++ src/pages/PostDetail.jsx
@@ -13,34 +13,32 @@ function PostDetail() {
   }, [id]);
 
   if (!post) {
-    return <Layout>Loading...</Layout>;
+    return "Loading...";
   }
 
   return (
-    <Layout>
-      <article>
-        <img src={post.thumbnailUrl} alt={post.title} />
-        <div className="mt-4 mb-2 mx-4">
-          <div className="flex justify-between items-center">
-            <time className="text-sm text-gray-500">
-              {new Date(post.createdAt).toLocaleDateString()}
-            </time>
-            <div className="flex gap-2">
-              {post.categories.map((category) => (
-                <span
-                  key={category}
-                  className="text-sm text-blue-600 border border-blue-600 px-2 py-1 rounded"
-                >
-                  {category}
-                </span>
-              ))}
-            </div>
+    <article>
+      <img src={post.thumbnailUrl} alt={post.title} />
+      <div className="mt-4 mb-2 mx-4">
+        <div className="flex justify-between items-center">
+          <time className="text-sm text-gray-500">
+            {new Date(post.createdAt).toLocaleDateString()}
+          </time>
+          <div className="flex gap-2">
+            {post.categories.map((category) => (
+              <span
+                key={category}
+                className="text-sm text-blue-600 border border-blue-600 px-2 py-1 rounded"
+              >
+                {category}
+              </span>
+            ))}
           </div>
-          <h1 className="text-2xl mt-2 mb-4">{post.title}</h1>
-          <div dangerouslySetInnerHTML={{ __html: post.content }} />
         </div>
-      </article>
-    </Layout>
+        <h1 className="text-2xl mt-2 mb-4">{post.title}</h1>
+        <div dangerouslySetInnerHTML={{ __html: post.content }} />
+      </div>
+    </article>
   );
 }
 
