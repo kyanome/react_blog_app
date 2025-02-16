057103d79a589375be65f76901d449d29ba52f5f
kyanome <yanome.keita@gmail.com>
2025-02-16 09:21:54 +0900

    remove ternary operator

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,16 @@
-import React, { useEffect, useState } from 'react';
 import { posts } from '../data/posts';
 import PostCard from '../components/PostCard';
 
 function Home() {
   return (
     <div className="max-w-3xl mx-auto my-10 px-4">
-      {posts.length > 0 ? (
-        <ul className="space-y-4">
-          {posts.map((post) => (
-            <li key={post.id}>
-              <PostCard post={post} />
-            </li>
-          ))}
-        </ul>
-      ) : null}
+      <ul className="space-y-4">
+        {posts.map((post) => (
+          <li key={post.id}>
+            <PostCard post={post} />
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
