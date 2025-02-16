33d9679fc40998ada60b5981840230f248fc3029
kyanome <yanome.keita@gmail.com>
2025-02-16 09:17:52 +0900

    change indentation from 4 to 2 spaces

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
-import { Route, Routes } from 'react-router-dom'
-import './App.css'
-import Header from './components/Header'
-import Home from './pages/Home'
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import Header from './components/Header';
+import Home from './pages/Home';
 
 function App() {
-
   return (
     <>
-      <Header/>
+      <Header />
       <Routes>
-        <Route path="/" element={<Home/>}></Route>
+        <Route path="/" element={<Home />}></Route>
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
-    <header className='bg-[#333] text-white px-6 py-6 font-bold flex justify-between items-center'>
-        <Link to="/" className='ml-4'>Blog</Link>
-        <Link to="/contact" className='ml-4'>お問い合わせ</Link>
+    <header className="bg-[#333] text-white px-6 py-6 font-bold flex justify-between items-center">
+      <Link to="/" className="ml-4">
+        Blog
+      </Link>
+      <Link to="/contact" className="ml-4">
+        お問い合わせ
+      </Link>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
